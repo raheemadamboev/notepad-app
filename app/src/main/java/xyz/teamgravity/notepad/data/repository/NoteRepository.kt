0755e85dcc3470a809468ebfc071eda870ba3929
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 14:53:18 +0500

    add note repository

---
+++ app/src/main/java/xyz/teamgravity/notepad/data/repository/NoteRepository.kt
@@ -0,0 +1,65 @@
+package xyz.teamgravity.notepad.data.repository
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import xyz.teamgravity.notepad.data.local.dao.NoteDao
+import xyz.teamgravity.notepad.data.mapper.toEntity
+import xyz.teamgravity.notepad.data.mapper.toModel
+import xyz.teamgravity.notepad.data.model.NoteModel
+
+class NoteRepository(
+    private val dao: NoteDao
+) {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // INSERT
+    ///////////////////////////////////////////////////////////////////////////
+
+    suspend fun insertNote(note: NoteModel) {
+        withContext(Dispatchers.IO) {
+            dao.insertNote(note.toEntity())
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // UPDATE
+    ///////////////////////////////////////////////////////////////////////////
+
+    suspend fun updateNote(note: NoteModel) {
+        withContext(Dispatchers.IO) {
+            dao.updateNote(note.toEntity())
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // DELETE
+    ///////////////////////////////////////////////////////////////////////////
+
+    suspend fun deleteNote(note: NoteModel) {
+        withContext(Dispatchers.IO) {
+            dao.deleteNote(note.toEntity())
+        }
+    }
+
+    suspend fun deleteNotes(notes: List<NoteModel>) {
+        withContext(Dispatchers.IO) {
+            dao.deleteNotes(notes.map { it.toEntity() })
+        }
+    }
+
+    suspend fun deleteAllNotes() {
+        withContext(Dispatchers.IO) {
+            dao.deleteAllNotes()
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // GET
+    ///////////////////////////////////////////////////////////////////////////
+
+    fun getAllNotes(): Flow<List<NoteModel>> {
+        return dao.getAllNotes().map { entities -> entities.map { it.toModel() } }
+    }
+}
