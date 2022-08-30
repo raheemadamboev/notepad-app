29c5aee3e9f37061b6f647e13dbd80a70257cb56
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 15:57:13 +0500

    add icon plain

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/component/image/IconPlain.kt
@@ -0,0 +1,23 @@
+package xyz.teamgravity.notepad.presentation.component.image
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconPlain(
+    @DrawableRes icon: Int,
+    @StringRes contentDescription: Int,
+) {
+    Icon(
+        painter = painterResource(id = icon),
+        contentDescription = stringResource(id = contentDescription),
+        modifier = Modifier.size(20.dp)
+    )
+}
