4db142580161e0885771ddce82afb2eec2dc63b2
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 15:51:23 +0500

    install navigation to main

--- app/src/main/java/xyz/teamgravity/notepad/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/activity/Main.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.notepad.presentation.navigation.Navigation
 import xyz.teamgravity.notepad.presentation.theme.NotepadTheme
 
 @AndroidEntryPoint
@@ -21,7 +22,7 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Navigation()
                 }
             }
         }
