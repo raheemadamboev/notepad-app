f1f7d5da9fee650943accaecb665e728d7ed8185
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 15:23:54 +0500

    install hilt android app and plant timber tree

---
+++ app/src/main/java/xyz/teamgravity/notepad/injection/app/App.kt
@@ -0,0 +1,20 @@
+package xyz.teamgravity.notepad.injection.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import xyz.teamgravity.notepad.BuildConfig
+import javax.inject.Inject
+
+@HiltAndroidApp
+class App : Application() {
+
+    @Inject
+    lateinit var tree: Timber.DebugTree
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(tree)
+    }
+}
