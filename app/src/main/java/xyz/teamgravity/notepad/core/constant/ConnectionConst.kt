6b41f421aaca4370c21e0749baeeba2b519fc22f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 14:58:39 +0500

    add constants for connections

---
+++ app/src/main/java/xyz/teamgravity/notepad/core/constant/ConnectionConst.kt
@@ -0,0 +1,34 @@
+package xyz.teamgravity.notepad.core.constant
+
+object ConnectionConst {
+
+    /**
+     * Support telegram link
+     */
+    const val SUPPORT_TELEGRAM = "https://t.me/raheem_adamboev"
+
+    /**
+     * Support mail
+     */
+    const val SUPPORT_MAIL = "raheemideadev@gmail.com"
+
+    /**
+     * To-Do package name
+     */
+    const val TODO_PACKAGE_NAME = "xyz.teamgravity.todo"
+
+    /**
+     * Gravity developer page
+     */
+    const val GRAVITY = "dev?id=5173702047357476752"
+
+    /**
+     * Url of Google Play Store detail page
+     */
+    const val PLAY_STORE_DETAIL_PAGE = "https://play.google.com/store/apps/details?id="
+
+    /**
+     * Url of source code on Github
+     */
+    const val GITHUB_SOURCE_CODE = "https://github.com/raheemadamboev/notepad-app"
+}
