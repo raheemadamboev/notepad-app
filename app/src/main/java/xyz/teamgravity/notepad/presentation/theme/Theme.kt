6952090edf30afc011066124bace8f77d3f39845
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 14:02:46 +0500

    setup material3 theme

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/theme/Theme.kt
@@ -0,0 +1,99 @@
+package xyz.teamgravity.notepad.presentation.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = GreenGrey30,
+    onPrimary = White,
+    primaryContainer = Green30,
+    onPrimaryContainer = White700,
+    inversePrimary = Green40,
+    secondary = DeepGreen80,
+    onSecondary = DeepGreen20,
+    secondaryContainer = GreenGrey30,
+    onSecondaryContainer = DeepGreen90,
+    tertiary = Violet80,
+    onTertiary = Violet20,
+    tertiaryContainer = Violet30,
+    onTertiaryContainer = Violet90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = DarkGray,
+    onBackground = White700,
+    surface = DarkGray200,
+    onSurface = White700,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = DarkGray200,
+    onSurfaceVariant = White700,
+    outline = GreenGrey80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Green40,
+    onPrimary = White,
+    primaryContainer = Green90,
+    onPrimaryContainer = Green10,
+    inversePrimary = Green80,
+    secondary = DeepGreen40,
+    onSecondary = White,
+    secondaryContainer = DeepGreen90,
+    onSecondaryContainer = DeepGreen10,
+    tertiary = Violet40,
+    onTertiary = White,
+    tertiaryContainer = Violet90,
+    onTertiaryContainer = Violet10,
+    error = Red40,
+    onError = White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = GreenGrey90,
+    onSurface = GreenGrey30,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = GreenGrey90,
+    onSurfaceVariant = GreenGrey30,
+    outline = GreenGrey50
+)
+
+@Composable
+fun NotepadTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
