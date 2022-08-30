480d394b9a72c94c7b4bb54cb6f5ba2347c58b44
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 15:52:28 +0500

    add screens

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/NoteAddScreen.kt
@@ -0,0 +1,12 @@
+package xyz.teamgravity.notepad.presentation.screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import xyz.teamgravity.notepad.presentation.navigation.MainNavGraph
+
+@MainNavGraph
+@Destination
+@Composable
+fun NoteAddScreen() {
+
+}
