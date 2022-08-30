d611fa31112fd0167b9a751364cfcf83cb2b9f3f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 19:50:53 +0500

    add notepad text field
    Took 1 minute

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/textfield/NotepadTextField.kt
@@ -0,0 +1,31 @@
+package xyz.teamgravity.notepad.presentation.textfield
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.notepad.R
+
+@Composable
+fun NotepadTextField(
+    title: String,
+    onTitleChange: (title: String) -> Unit,
+    body: String,
+    onBodyChange: (body: String) -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        NoteTextField(
+            value = title,
+            onValueChange = onTitleChange,
+            placeholder = R.string.title
+        )
+        NoteTextField(
+            value = body,
+            onValueChange = onBodyChange,
+            placeholder = R.string.note,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
