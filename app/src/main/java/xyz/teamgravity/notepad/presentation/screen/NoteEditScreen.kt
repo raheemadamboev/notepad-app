783783599d3718327139b9b9818f98fed92c9a46
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 18:38:27 +0500

    setup node edit screen nav args
    Took 25 seconds

--- app/src/main/java/xyz/teamgravity/notepad/presentation/screen/NoteEditScreen.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/NoteEditScreen.kt
@@ -2,11 +2,16 @@ package xyz.teamgravity.notepad.presentation.screen
 
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
+import xyz.teamgravity.notepad.data.model.NoteModel
 import xyz.teamgravity.notepad.presentation.navigation.MainNavGraph
 
 @MainNavGraph
-@Destination
+@Destination(navArgsDelegate = NoteEditScreenNavArgs::class)
 @Composable
 fun NoteEditScreen() {
 
 }
+
+data class NoteEditScreenNavArgs(
+    val note: NoteModel
+)
