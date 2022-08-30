9f9b5953874fed66768d9a6d2caaff8278815eea
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 18:45:57 +0500

    add note list view model
    Took 1 minute

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/viewmodel/NoteListViewModel.kt
@@ -0,0 +1,68 @@
+package xyz.teamgravity.notepad.presentation.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import xyz.teamgravity.notepad.data.model.NoteModel
+import xyz.teamgravity.notepad.data.repository.NoteRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class NoteListViewModel @Inject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    var notes: List<NoteModel> by mutableStateOf(emptyList())
+        private set
+
+    var expanded: Boolean by mutableStateOf(false)
+        private set
+
+    var deleteAllDialog: Boolean by mutableStateOf(false)
+        private set
+
+    init {
+        observe()
+    }
+
+    fun onMenuExpanded() {
+        expanded = true
+    }
+
+    fun onMenuCollapsed() {
+        expanded = false
+    }
+
+    fun onDeleteAllDialogShow() {
+        deleteAllDialog = true
+        onMenuCollapsed()
+    }
+
+    fun onDeleteAllDialogDismiss() {
+        deleteAllDialog = false
+    }
+
+    fun onDeleteAll() {
+        viewModelScope.launch(NonCancellable) {
+            repository.deleteAllNotes()
+        }
+    }
+
+    private fun observe() {
+        observeNotes()
+    }
+
+    private fun observeNotes() {
+        viewModelScope.launch {
+            repository.getAllNotes().collectLatest { notes ->
+                this@NoteListViewModel.notes = notes
+            }
+        }
+    }
+}
