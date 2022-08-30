0c18bb3d2ec9935f734be2ef2332218438176ca5
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-30 14:45:27 +0500

    add constants for note database

---
+++ app/src/main/java/xyz/teamgravity/notepad/data/local/constant/NoteDatabaseConst.kt
@@ -0,0 +1,21 @@
+package xyz.teamgravity.notepad.data.local.constant
+
+object NoteDatabaseConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "notepad_database"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table note
+     * Entity -> [xyz.teamgravity.notepad.data.local.entity.NoteEntity]
+     * Model -> [xyz.teamgravity.notepad.data.model.NoteModel]
+     */
+    const val TABLE_NOTE = "note_table"
+}
