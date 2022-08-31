cea73a459ef28b1270d602a72640a85e47a18b7f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-31 14:13:46 +0500

    build support screen
    Took 24 seconds

--- app/src/main/java/xyz/teamgravity/notepad/presentation/screen/support/SupportScreen.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/support/SupportScreen.kt
@@ -2,11 +2,19 @@ package xyz.teamgravity.notepad.presentation.screen.support
 
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.notepad.presentation.component.misc.WindowInfo
+import xyz.teamgravity.notepad.presentation.component.misc.rememberWindowInfo
 import xyz.teamgravity.notepad.presentation.navigation.MainNavGraph
 
 @MainNavGraph
 @Destination
 @Composable
-fun SupportScreen() {
-
+fun SupportScreen(
+    navigator: DestinationsNavigator,
+) {
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
