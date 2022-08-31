10e4a8c3e1c8e142197a7e152ee036292693383c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-31 11:00:17 +0500

    build about screen
    Took 28 minutes

---
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/about/AboutLandscapeScreen.kt
@@ -0,0 +1,126 @@
+package xyz.teamgravity.notepad.presentation.screen.about
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import xyz.teamgravity.notepad.BuildConfig
+import xyz.teamgravity.notepad.R
+import xyz.teamgravity.notepad.core.util.Helper
+import xyz.teamgravity.notepad.presentation.component.button.IconButtonPlain
+import xyz.teamgravity.notepad.presentation.component.text.TextPlain
+import xyz.teamgravity.notepad.presentation.component.topbar.TopBar
+
+@Composable
+fun AboutLandscapeScreen(
+    onBackButtonClick: () -> Unit,
+) {
+
+    val context = LocalContext.current
+
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = { TextPlain(id = R.string.app_name) },
+                navigationIcon = {
+                    IconButtonPlain(
+                        onClick = onBackButtonClick,
+                        icon = Icons.Default.ArrowBack,
+                        contentDescription = R.string.cd_back_button
+                    )
+                }
+            )
+        }
+    ) { padding ->
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            val (appI, appNameT, appVersionT, oneS, companyC) = createRefs()
+            val oneG = createGuidelineFromStart(0.5F)
+
+            Image(
+                painter = painterResource(id = R.drawable.icon),
+                contentDescription = stringResource(id = R.string.cd_app_icon),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.constrainAs(appI) {
+                    width = Dimension.value(180.dp)
+                    height = Dimension.value(180.dp)
+                    linkTo(start = parent.start, end = oneG)
+                    linkTo(top = parent.top, bottom = parent.bottom)
+                }
+            )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                textAlign = TextAlign.Center,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Black,
+                modifier = Modifier.constrainAs(appNameT) {
+                    width = Dimension.fillToConstraints
+                    linkTo(start = oneG, end = parent.end)
+                }
+            )
+            Text(
+                text = BuildConfig.VERSION_NAME,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.constrainAs(appVersionT) {
+                    width = Dimension.fillToConstraints
+                    linkTo(start = oneG, end = parent.end)
+                }
+            )
+            Spacer(
+                modifier = Modifier.constrainAs(oneS) {
+                    height = Dimension.value(16.dp)
+                    linkTo(start = oneG, end = parent.end)
+                }
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .clickable { Helper.viewGravityPage(context) }
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
+                    .constrainAs(companyC) {
+                        linkTo(start = oneG, end = parent.end)
+                    }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.gravity),
+                    contentDescription = stringResource(id = R.string.cd_company_logo),
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(20.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    text = stringResource(id = R.string.raheem),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 12.sp,
+                )
+            }
+
+            createVerticalChain(appNameT, appVersionT, oneS, companyC, chainStyle = ChainStyle.Packed)
+        }
+    }
+}
--- app/src/main/java/xyz/teamgravity/notepad/presentation/screen/about/AboutScreen.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/about/AboutScreen.kt
@@ -2,11 +2,19 @@ package xyz.teamgravity.notepad.presentation.screen.about
 
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.notepad.presentation.component.misc.WindowInfo
+import xyz.teamgravity.notepad.presentation.component.misc.rememberWindowInfo
 import xyz.teamgravity.notepad.presentation.navigation.MainNavGraph
 
 @MainNavGraph
 @Destination
 @Composable
-fun AboutScreen() {
-
+fun AboutScreen(
+    navigator: DestinationsNavigator,
+) {
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> AboutPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> AboutLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
