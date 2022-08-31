dfa9c0d303a527207c3e0a26cbee9d4cd56a0f2d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-31 15:46:59 +0500

    add icon plain
    Took 25 seconds

--- app/src/main/java/xyz/teamgravity/notepad/presentation/component/image/IconPlain.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/component/image/IconPlain.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,6 +18,18 @@ fun IconPlain(
 ) {
     Icon(
         painter = painterResource(id = icon),
+        contentDescription = stringResource(id = contentDescription),
+        modifier = Modifier.size(20.dp)
+    )
+}
+
+@Composable
+fun IconPlain(
+    icon: ImageVector,
+    @StringRes contentDescription: Int,
+) {
+    Icon(
+        imageVector = icon,
         contentDescription = stringResource(id = contentDescription),
         modifier = Modifier.size(20.dp)
     )
