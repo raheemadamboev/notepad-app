336704884a5245299c52750cf7c89f605ab03301
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-01 15:31:25 +0500

    final refactoring
    Took 5 minutes

--- app/src/main/java/xyz/teamgravity/notepad/data/model/NoteModel.kt
+++ app/src/main/java/xyz/teamgravity/notepad/data/model/NoteModel.kt
@@ -8,7 +8,7 @@ import java.util.*
 data class NoteModel(
     val id: Long = 0,
     val title: String,
-    val body: String = "",
+    val body: String,
     val created: Date = Date(),
     val edited: Date = Date(),
 ) : Parcelable
