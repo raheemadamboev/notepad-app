e5334af97701823baf8a187310b82ae85ceb1042
raheemadamboev <vividricemaryandmax@gmail.com>
2023-03-04 14:07:13 +0500

    [1.1.8] Removed unnecessary field;
    Took 10 seconds

--- app/src/main/java/xyz/teamgravity/notepad/core/constant/ConnectionConst.kt
+++ app/src/main/java/xyz/teamgravity/notepad/core/constant/ConnectionConst.kt
@@ -13,11 +13,6 @@ object ConnectionConst {
     const val SUPPORT_MAIL = "raheemideadev@gmail.com"
 
     /**
-     * Gravity developer page
-     */
-    const val GRAVITY = "dev?id=5173702047357476752"
-
-    /**
      * Url of Google Play Store detail page
      */
     const val PLAY_STORE_DETAIL_PAGE = "https://play.google.com/store/apps/details?id="
