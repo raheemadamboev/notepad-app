a32c013eed39687ceda76376c1144eb3128c31dc
raheem <raheemideadev@gmail.com>
2025-05-10 01:15:50 +0500

    [1.2.0] Migrated Gradle files from Groovy to Kotlin Script #20; Integrated Gravity's core-sdk-android and core-sdk-compose #21; Added new plugins (Compose, Parcelize, Room) #31; Upgraded Gradle, AGP, Kotlin, Plugins, dependencies versions to the latest #32;

---
+++ app/build.gradle.kts
@@ -0,0 +1,144 @@
+plugins {
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.parcelize)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.gms)
+    alias(libs.plugins.crashlytics)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.room)
+}
+
+android {
+    namespace = "xyz.teamgravity.notepad"
+    compileSdk = libs.versions.sdk.compile.get().toInt()
+
+    defaultConfig {
+        applicationId = "xyz.teamgravity.notepad"
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
+        versionCode = 12
+        versionName = "1.2.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        ndk {
+            debugSymbolLevel = "FULL"
+            abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a", "x86_64", "x86"))
+        }
+
+        bundle {
+            language {
+                enableSplit = false
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+        debug {
+            applicationIdSuffix = ".debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.java.target.get()
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
+}
+
+dependencies {
+
+    // compose
+    implementation(platform(libs.compose))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.icons)
+
+    // compose activity
+    implementation(libs.compose.activity)
+
+    // compose lifecycle
+    implementation(libs.compose.lifecycle)
+
+    // compose viewmodel
+    implementation(libs.compose.viewmodel)
+
+    // compose hilt
+    implementation(libs.compose.hilt)
+
+    // compose paging
+    implementation(libs.compose.paging)
+
+    // core
+    implementation(libs.core)
+
+    // splash
+    implementation(libs.splash)
+
+    // collections
+    implementation(libs.collections)
+
+    // firebase
+    implementation(platform(libs.firebase))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.messaging)
+
+    // hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+
+    // coroutines
+    implementation(libs.coroutines)
+    implementation(libs.coroutines.android)
+
+    // room
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    implementation(libs.room.paging)
+    ksp(libs.room.compiler)
+
+    // destinations
+    implementation(libs.destinations)
+    ksp(libs.destinations.compiler)
+
+    // timber
+    implementation(libs.timber)
+
+    // gravity core
+    implementation(libs.gravity.core)
+    implementation(libs.gravity.core.compose)
+
+    // gravity pinlock
+    implementation(libs.gravity.pinlock)
+}
