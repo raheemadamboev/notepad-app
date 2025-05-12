72a841592068e1fabc6105d7d008aaf89f762e96
raheem <raheemideadev@gmail.com>
2025-05-12 22:13:11 +0500

    [1.2.0] Install androidx.splash screen #34;

--- app/src/main/java/xyz/teamgravity/notepad/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/activity/Main.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.notepad.presentation.navigation.Navigation
 import xyz.teamgravity.notepad.presentation.theme.NotepadTheme
@@ -15,6 +16,7 @@ import xyz.teamgravity.notepad.presentation.theme.NotepadTheme
 class Main : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             NotepadTheme {
