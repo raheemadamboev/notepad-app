5da19530b2c4dce27128c0fbc89962e5ddaca66d
raheem <raheemideadev@gmail.com>
2025-06-06 22:47:42 +0500

    [1.2.3] Removed unused context from About screen;

--- app/src/main/java/xyz/teamgravity/notepad/presentation/screen/about/AboutLandscapeScreen.kt
+++ app/src/main/java/xyz/teamgravity/notepad/presentation/screen/about/AboutLandscapeScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,8 +39,6 @@ import xyz.teamgravity.notepad.presentation.component.topbar.TopBar
 fun AboutLandscapeScreen(
     onBackButtonClick: () -> Unit
 ) {
-    val context = LocalContext.current
-
     Scaffold(
         topBar = {
             TopBar(
